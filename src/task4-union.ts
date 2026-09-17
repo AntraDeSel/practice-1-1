2e47f44466492ee7c077cf888e41934469ca9ba3
unknown <olya@HOME-PC1.home.local>
2026-09-17 21:07:49 +0500

    практика выполнена не полностью, только до 5 задания вкючительно Акимова о.а.

--- src/task4-union.ts
+++ src/task4-union.ts
@@ -6,13 +6,22 @@ export type Currency = "RUB" | "USD" | "EUR";
 // 1 EUR = 100 RUB
 
 export function convert(amount: number, from: Currency, to: Currency): number {
-  // Напишите код здесь
-  // Алгоритм:
-  // 1. Если from === to, вернуть amount
-  // 2. Иначе конвертировать from -> RUB, потом RUB -> to
+  if (from === to) {
+    return amount;
+  }
+  else {
+  const rates: Record<Currency, number> = {
+    RUB: 1,
+    USD: 90,
+    EUR: 100,
+    }
+    const amountInRub = amount * rates[from];
+    return amountInRub / rates[to];
+  
+}
+    
 }
 
 export function formatCurrency(amount: number, currency: Currency): string {
-  // Вернуть строку вида "100 RUB" или "50.5 USD"
-  // Напишите код здесь
+  return `${amount} ${currency}`;
 }
